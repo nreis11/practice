d7711d069e071024441e897fe1b150e19bac9e22
ricebullet <nicholasreis@gmail.com>
2015-10-07 16:21:15 -0700

    Optimized
    Plus reverse_2

--- reverse.py
+++ reverse.py
@@ -1,10 +1,19 @@
-def reverse(n):
-    letters = list(n)
+def reverse(word):
+    letters = list(word)
+    r_list = letters
     i = 0
-    for items in range(len(letters)):
-        end = letters.pop()
-        letters.insert(i,end)
+    for items in range(len(r_list)):
+        end = r_list.pop()
+        r_list.insert(i,end)
         i += 1
     delimiter = ''
-    processed = delimiter.join(letters)
+    processed = delimiter.join(r_list)
     print processed
+
+def reverse_2(word):
+    modified = ''
+    i = -1
+    for item in range(len(word)):
+        modified = modified + word[i]
+        i -= 1
+    print modified
